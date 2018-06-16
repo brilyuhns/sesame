9a6c4e276b7794793bac882c0570fef6a52936cd
Divya Kunnath <brilyuhns@Divyas-MacBook-Pro.local>
2018-06-16 13:30:38 +0800

    Add logging and ability to get new connection

--- lib/vault_activerecord.rb
+++ lib/vault_activerecord.rb
@@ -1,8 +1,24 @@
 require "vault_activerecord/version"
+require 'vault'
+require 'logger'
 
 module VaultActiverecord
+  $database_creds = {}
+  class << self  
+    attr_accessor :logger
+
+    def logger 
+      @logger = ::Logger.new(STDOUT).tap do |log|
+        log.progname = 'vault_activerecord' 
+      end
+    end
+  end
 
   def self.patch
+    $database_creds = yield
+    logger.info 'New credentials set'
   end
   # Your code goes here...
 end
+
+
--- spec/vault_activerecord_spec.rb
+++ spec/vault_activerecord_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe VaultActiverecord do
   it "has a patch method" do
     expect(VaultActiverecord.respond_to?('patch')).to eq(true)
   end
+
+  describe :patch do 
+  	it 'calls the block passed' do 
+  	  VaultActiverecord.patch do { username: 'pg_username' } end
+  	  expect($database_creds).to eq({username: 'pg_username'})
+  	  VaultActiverecord.patch do { username: 'pg_username2' } end
+  	  expect($database_creds).to eq({username: 'pg_username2'})
+  	end
+  end
 end
